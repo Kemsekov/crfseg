fa9812a44ec8e7c713f82cbb5a593b8d444ab6e2
Vlad Kemsekov <pikova98@bk.ru>
2024-12-19 15:51:40 +0500

    Update torchscript_model.py
    remove unused tqdm

--- crfseg/torchscript_model.py
+++ crfseg/torchscript_model.py
@@ -4,9 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .utils import tqdm_if
-
-
 class CRFTorchScript(nn.Module):
     """
     Compilable by torchscript version of CRF module.
